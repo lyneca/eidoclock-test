e916732139de45637da22fa5ff15f253f2e6bdb9
Lyneca <atraguliauniliantauthr@gmail.com>
2019-01-23 18:15:47 +1100

    Initial commit

---
+++ precache-manifest.4b2ad05fe786d22172fd0791dfab2c9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b8e8bcd84cb5bf5f0d2",
+    "url": "/static/css/main.72d4226d.chunk.css"
+  },
+  {
+    "revision": "1b8e8bcd84cb5bf5f0d2",
+    "url": "/static/js/main.1b8e8bcd.chunk.js"
+  },
+  {
+    "revision": "d2cf9a2733a3d48b32b0",
+    "url": "/static/js/1.d2cf9a27.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "60c866748ff15f5b347fdba64596b1b1",
+    "url": "/static/media/open_sans.60c86674.woff2"
+  },
+  {
+    "revision": "29b00ebcf93fda46ac9957fa4816eafd",
+    "url": "/static/media/inconsolata.29b00ebc.ttf"
+  },
+  {
+    "revision": "93c92acc87569f6e5d64b4800e310160",
+    "url": "/index.html"
+  }
+];
