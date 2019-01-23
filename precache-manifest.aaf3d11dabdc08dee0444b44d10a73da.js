62c8555e6237e72856dc8c03abb2b60bc5a917de
Lyneca <atraguliauniliantauthr@gmail.com>
2019-01-23 19:00:03 +1100

    Updates

---
+++ precache-manifest.aaf3d11dabdc08dee0444b44d10a73da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "65d0db68ad2e81f08ee0",
+    "url": "/eidoclock-test/static/css/main.955ed790.chunk.css"
+  },
+  {
+    "revision": "65d0db68ad2e81f08ee0",
+    "url": "/eidoclock-test/static/js/main.65d0db68.chunk.js"
+  },
+  {
+    "revision": "d2cf9a2733a3d48b32b0",
+    "url": "/eidoclock-test/static/js/1.d2cf9a27.chunk.js"
+  },
+  {
+    "revision": "10542f33c7d12a004b6e",
+    "url": "/eidoclock-test/static/js/runtime~main.10542f33.js"
+  },
+  {
+    "revision": "60c866748ff15f5b347fdba64596b1b1",
+    "url": "/eidoclock-test/static/media/open_sans.60c86674.woff2"
+  },
+  {
+    "revision": "29b00ebcf93fda46ac9957fa4816eafd",
+    "url": "/eidoclock-test/static/media/inconsolata.29b00ebc.ttf"
+  },
+  {
+    "revision": "f97b8126fff7a7b2f14c00f4d637ddad",
+    "url": "/eidoclock-test/index.html"
+  }
+];
